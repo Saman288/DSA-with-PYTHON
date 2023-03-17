2be07c2ab2a4a61fe9ae5fa826a9171a71478e35
Saman288 <122076165+Saman288@users.noreply.github.com>
2023-03-17 12:00:50 +0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+List1=[0,9,7,5,2,1]
+print(List1)
+
+List2=[[1,2],4,5]
+print(List2)
+
+List3=['Joseph','Glem','Sally']
+print(List3)
+
+List4=['Saman',6,9,[3,4],90,'Khan']
+print(List4)
+
+List5=['Saman','Female',20,'Rs & GIS','IST',[89,0,7]]
+print(List5)
+
+List6=[[2,1],'Saman',40]
+print(List6[0],List6[1],List6[2])
+
+List7=[(1,2),9,{8:5},23,{4:"Keqin-Li"}]
+print(List7)
+
+
