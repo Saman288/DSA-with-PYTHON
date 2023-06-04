a84700a6b036b5512a9c010ddfe01747f8a38ae0
Saman288 <122076165+Saman288@users.noreply.github.com>
2023-06-04 06:43:43 -0700

    Add files via upload

---
+++ practemp.py
@@ -0,0 +1,4 @@
+celsius=float(input("Temp value in degree celsius:"))
+
+Fahrenheit=(celsius * 1.8) + 32
+print('%0.1f degree celsius is equal to %0.1f degree fahrenheit' %(celsius,Fahrenheit))
---
+++ practempp.py
@@ -0,0 +1,3 @@
+temperature = float(input("Enter temperature in Fahrenheit:"))
+celsius=(temperature -32) *5/9
+print ("Temperature in celsius", celsius)
