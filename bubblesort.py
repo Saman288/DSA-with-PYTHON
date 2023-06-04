e806c7c6a1218e3549a550afe8ac6ce922ff63c7
Saman288 <122076165+Saman288@users.noreply.github.com>
2023-06-04 04:01:59 -0700

    Add files via upload

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,24 @@
-def bubbleSort(arr):
+def bubble_sort(arr):
     n = len(arr)
-    # Traverse through all array elements
-    for i in range( n-1):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j + 1] :
+
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+            # Traverse the array from 0 to n - i - 1
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-arr= [-2,45,0,11,-9]
-print(arr)
-bubbleSort(arr)
-print(arr)
+
+# Create an unsorted list
+my_list = [64, 34, 25, 12, 22, 11, 90]
+
+# Display the unsorted list
+print("Unsorted List:")
+print(my_list)
+
+# Sort the list using bubble sort
+bubble_sort(my_list)
+
+# Display the sorted list
+print("\nSorted List:")
+print(my_list)
---
+++ practice.py
@@ -0,0 +1,10 @@
+a=[]
+
+n=int(input("Enter elements:"))
+
+for i in range(n):
+    a.append(int(input("Enter element:")))
+
+print("List:")
+for elements in a:
+    print(elements)
